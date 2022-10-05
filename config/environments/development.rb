a40da9d15418a372ed9a2c7264e0ce7ca7f73c43
ukah <ukah@live.jp>
2022-10-05 11:24:30 +0000

    fix goorm env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+	
+  # なんかこれ入れないとだめっぽい
+  config.hosts << "rails-test-ehizj.run.goorm.io"
 end
